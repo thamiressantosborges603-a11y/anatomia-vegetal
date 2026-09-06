a2ec9c9330a40d547b6823a1e5c35dc37bfe28a3
thamiressantosborges603-a11y <thamiressantosborges603@gmail.com>
2026-09-06 00:20:26 -0300

    Create AnatomiaVegetalApplication.java

---
+++ src/main/java/br/com/anatomiavegetal/AnatomiaVegetalApplication.java
@@ -0,0 +1,12 @@
+package br.com.anatomiavegetal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnatomiaVegetalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnatomiaVegetalApplication.class, args);
+    }
+}
